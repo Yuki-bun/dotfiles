7645e8cf8180fb11dee2d2b702ceb5d05ed59b7c
Yuki-bun <myu7253@gmail.com>
2024-05-19 15:16:55 +0900

    delete config

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.cmd("set expandtab")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set tabstop=2")
