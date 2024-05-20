1339b2d75bb52852ca5be4ab6586dd5cdb7bd073
Yuki-bun <myu7253@gmail.com>
2024-05-20 13:30:04 +0900

    delete lazygit plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -42,28 +42,4 @@ return {
 	{
 		"tpope/vim-fugitive",
 	},
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		-- setting the keybinding for LazyGit with 'keys' is recommended in
-		-- order to load the plugin when the command is run for the first time
-		keys = {
-			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
-		config = function()
-			if vim.fn.executable("nvr") == 1 then
-				vim.env.GIT_EDITOR = "nvr --remote-tab-wait +'set bufhidden=delete'"
-			end
-		end,
-	},
 }
