115af5cc620e3a6efd18aa2fdcecd3b170848e2a
Yuki-bun <myu7253@gmail.com>
2024-05-20 11:54:09 +0900

    change to gitsign

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,40 +1,32 @@
 return {
-	{
-		"f-person/git-blame.nvim",
-	},
-	{
-		"tanvirtin/vgit.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-    config = function ()
-      require("vgit").setup()
-    end
-	},
-	{
-		"echasnovski/mini.diff",
-		version = false,
-		config = function()
-			require("mini.diff").setup()
-		end,
-	},
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		-- setting the keybinding for LazyGit with 'keys' is recommended in
-		-- order to load the plugin when the command is run for the first time
-		keys = {
-			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
-	},
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+    config = function()
+      if vim.fn.executable("nvr") == 1 then
+        vim.env.GIT_EDITOR = "nvr --remote-tab-wait +'set bufhidden=delete'"
+      end
+    end,
+  },
 }
