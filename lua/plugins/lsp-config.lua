0414f3b8088c9699afb4e9babb779c128f00d1bd
Yuki-bun <myu7253@gmail.com>
2024-05-23 11:57:27 +0900

    feat: add global vim lsp support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,14 +16,20 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			-- IMPORTANT: make sure to setup neodev BEFORE lspconfig
+			require("neodev").setup({})
 			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.tsserver.setup({})
+			local on_attach = function(client, bufnr)
+				local navbuddy = require("nvim-navbuddy")
+				navbuddy.attach(client, bufnr)
+			end
+			lspconfig.lua_ls.setup({ capabilities = capabilities, on_attach = on_attach })
+			lspconfig.tsserver.setup({ capabilities = capabilities, on_attach = on_attach })
 			lspconfig.html.setup({})
 			lspconfig.tailwindcss.setup({})
-			lspconfig.pyright.setup({})
+			lspconfig.pyright.setup({ capabilities = capabilities, on_attach = on_attach })
 		end,
 	},
 }
