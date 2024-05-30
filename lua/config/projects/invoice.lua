e2aba38c2d75e10d304f61d6ff1caa7d2e0768da
Yuki-bun <myu7253@gmail.com>
2024-05-30 09:38:27 +0900

    fix: remove invoice setting

--- lua/config/projects/invoice.lua
+++ lua/config/projects/invoice.lua
@@ -1,29 +1,23 @@
 local null_ls = require("null-ls")
 
 local remove_formatter = function(formatter)
-	local builtins = null_ls.builtins.formatting
-	local sources = {}
-	-- P(builtins)
-	for key, source in pairs(builtins) do
-		if key ~= formatter then
-			table.insert(sources, source)
-		else
-			print("removed", formatter)
-		end
-	end
-	return sources
+  local builtins = null_ls.builtins.formatting
+  local sources = {}
+  -- P(builtins)
+  for key, source in pairs(builtins) do
+    if key ~= formatter then
+      table.insert(sources, source)
+    else
+      print("removed", formatter)
+    end
+  end
+  return sources
 end
 
 vim.api.nvim_create_augroup("Invoice", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = "*.tsx",
-	group = "Invoice",
-	desc = "remove prettier formatting capabilitis",
-	callback = function()
-		print("activated")
-		local sources = remove_formatter("prettier")
-		null_ls.setup({
-			sources = {},
-		})
-	end,
+  pattern = "*.tsx",
+  group = "Invoice",
+  desc = "remove prettier formatting capabilitis",
+  callback = function() end,
 })
