fb936a33fa1403a6d90d4097bd2a1c3a14ae1efb
Yuki-bun <myu7253@gmail.com>
2024-06-16 07:10:59 +0900

    feat: move to new repo?

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,13 @@
 return {
-  "github/copilot.vim",
-  config = function()
-    vim.g.copilot_no_tab_map = true
-    vim.keymap.set(
-      "i",
-      "<C-g>",
-      'copilot#Accept("<CR>")',
-      { silent = true, expr = true, script = true, replace_keycodes = false }
-    )
-  end,
+	"github/copilot.vim",
+	config = function()
+		vim.g.copilot_no_tab_map = true
+
+		vim.keymap.set(
+			"i",
+			"<C-g>",
+			'copilot#Accept("<CR>")',
+			{ silent = true, expr = true, script = true, replace_keycodes = false }
+		)
+	end,
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,26 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		vim.lsp.buf.format({ timeout_ms = 8001 })
+  "jose-elias-alvarez/null-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    vim.lsp.buf.format({ timeout_ms = 80001 })
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.eslint_d,
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.code_actions.eslint_d,
-				null_ls.builtins.formatting.autoflake,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-			},
-		})
-	end,
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.eslint_d,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.code_actions.eslint_d,
+        null_ls.builtins.formatting.autoflake,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        -- null_ls.builtins.diagnostics.swiftlint,
+        null_ls.builtins.formatting.swiftformat,
+        null_ls.builtins.formatting.swiftlint,
+      },
+    })
+  end,
 }
