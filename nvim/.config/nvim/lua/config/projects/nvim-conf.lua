3e0c8e5be3de742101c69f9053e1ad27df2b7dd5
Yuki-bun <myu7253@gmail.com>
2024-06-26 09:15:17 +0900

    remove auto format autocomand

--- nvim/.config/nvim/lua/config/projects/nvim-conf.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_augroup("auto-format", { clear = true })
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = "auto-format",
-  desc = "auto save",
-  callback = function()
-    vim.lsp.buf.format()
-  end,
-})
