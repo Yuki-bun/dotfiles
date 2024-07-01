be74765f0c3d2851eb85f4ec24f4c0c2becfcb2c
Yuto Maruta <115913823+Yuki-bun@users.noreply.github.com>
2024-07-01 01:30:42 +0000

    change logging

--- install.sh
+++ install.sh
@@ -1,44 +1,62 @@
-echo "----------Starting Setup----------"
+print() {
+    local message="$1"
+    local width=${2:-60}
+    local message_length=${#message}
+    local dash_length=$(( (width - message_length - 2) / 2 ))
 
-echo "Updating apt"
+    printf '%*s' "$dash_length" '' | tr ' ' -
+    printf ' %s ' "$message"
+    printf '%*s' "$dash_length" '' | tr ' ' -
+
+    if [ $(( (width - message_length - 2) % 2 )) -ne 0 ]; then
+        printf '-'
+    fi
+
+    echo
+}
+
+
+print "Starting Setup"
+
+print "Updating apt"
 sudo apt update
 
-echo "Installing Nvim"
+print "Installing nvim"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 sudo mv squashfs-root / && sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
-echo "Installing tmux"
+print "Installing tmux"
 sudo apt install tmux -y
 
-echo "Installing stow"
+print "Installing stow"
 sudo apt install stow -y
 
-echo "Installing fzf"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-yes y | ~/.fzf/install
-source ~/.bashrc
+print "Installing fzf"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.local/bin/.fzf
+~/.local/bin/.fzf/install --xdg --key-bindings --completion --update-rc
+source ~/.config/fzf/fzf.bash
 
-echo "Installing lazygit"
+print "Installing lazygit"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 
-echo "Installing fortune for tmux"
+print "Installing fortune for tmux"
 sudo apt install fortune -y
 
-echo "Install zoxide"
+print "Install zoxide"
 conda install -c conda-forge zoxide -y
 eval "$(zoxide init bash)"
 
 DOTFILES_DIR="/workspaces/.codespaces/.persistedshare/dotfiles"
 
-echo "Setting up simlinks"
+print "Setting up simlinks"
 ln -s $DOTFILES_DIR/nvim/.config/nvim ~/.config/nvim
 ln -s $DOTFILES_DIR/tmux/.tmux.conf ~/.tmux.conf
 
 
 
-echo "----------Setup Complete----------"
+print "SetuphComplete"
