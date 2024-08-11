3a0c2dc0d21c6dedb81698c3d46d71785c29f91d
Yuki-bun <myu7253@gmail.com>
2024-08-12 05:40:37 +0900

    add html pair

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,15 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	config = true,
-	-- use opts = {} for passing setup options
-	-- this is equalent to setup({}) function
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
 }
