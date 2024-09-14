8b7c6732c9f0b0803e80a8d73cce2d6c40596945
Yuki-bun <myu7253@gmail.com>
2024-09-14 18:44:03 +0900

    change: show file path

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -7,6 +7,9 @@ return {
 		require("oil").setup({
 			columns = { "icon" },
 			view_options = { show_hidden = true },
+			win_options = {
+				winbar = "%{v:lua.require('oil').get_current_dir()}",
+			},
 		})
 
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { noremap = true, desc = "edit dir" })
