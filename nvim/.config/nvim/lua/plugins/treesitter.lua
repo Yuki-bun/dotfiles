e264f43991c704151ce8497a79147c2b34b88f83
Yuki-bun <myu7253@gmail.com>
2024-10-08 16:35:25 +0900

    add tsc comment support

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -23,6 +23,18 @@ return {
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
+			context_commentstring = {
+				config = {
+					javascript = {
+						__default = "// %s",
+						jsx_element = "{/* %s */}",
+						jsx_fragment = "{/* %s */}",
+						jsx_attribute = "// %s",
+						comment = "// %s",
+					},
+					typescript = { __default = "// %s", __multiline = "/* %s */" },
+				},
+			},
 		})
 	end,
 }
