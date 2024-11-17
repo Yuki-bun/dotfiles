2f276a09963fd42fcb9b278c79cf8c0193831c63
Yuki-bun <myu7253@gmail.com>
2024-11-17 21:26:04 +0900

    remove unnecesary lines

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,19 +1,11 @@
 return {
 	"ellisonleao/gruvbox.nvim",
 	priority = 1000,
-	config = function()
-		require("gruvbox").setup({})
-		vim.o.background = "dark" -- or "light" for light mode
-		vim.cmd([[colorscheme gruvbox]])
-	end,
 	{
 		"bluz71/vim-moonfly-colors",
 		name = "moonfly",
 		lazy = false,
 		priority = 1000,
-		config = function()
-			vim.cmd([[colorscheme moonfly]])
-		end,
 	},
 	{ "rebelot/kanagawa.nvim" },
 	{ "rose-pine/neovim", name = "rose-pine" },
